c09a4de5af626958cf75f579098ce2ac96478e73
Nicolas Berces <>
2018-05-09 21:44:54 +1000

    Adding DateInterval class and refactoring DateTime class.

---
+++ src/DateInterval.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NBerces\PHPUtils;
+
+use DateInterval as BaseDateInterval;
+
+class DateInterval extends BaseDateInterval
+{
+    public static function toSpecification(DateInterval $interval)
+    {
+        $dateElements = array_filter(
+            [
+                'Y' => $interval->y,
+                'M' => $interval->m,
+                'D' => $interval->d
+            ]
+        );
+
+        $timeElements = array_filter(
+            [
+                'H' => $interval->h,
+                'M' => $interval->i,
+                'S' => $interval->s
+            ]
+        );
+
+        $str = 'P';
+
+        foreach ($dateElements as $period => $duration) {
+            $str .= $duration . $period;
+        }
+
+        if (!empty($timeElements)) {
+            $str .= 'T';
+
+            foreach ($timeElements as $period => $duration) {
+                $str .= $duration . $period;
+            }
+        }
+
+        return $str;
+    }
+
+    public function __toString()
+    {
+        return self::toSpecification($this);
+    }
+}
--- src/DateTime.php
+++ src/DateTime.php
@@ -2,76 +2,86 @@
 
 namespace NBerces\PHPUtils;
 
-class DateTime extends \DateTime
+use DateTime as BaseDateTime;
+
+class DateTime extends BaseDateTime
 {
     const FORMAT_HTML5_DATE = '~html5date';
     const FORMAT_HTML5_DATETIME = '~html5datetime';
     const FORMAT_MYSQL = '~mysql';
     const FORMAT_RELATIVE = '~';
+    const FORMAT_TIMEAGO = '~';
     const FORMAT_SOLR = '~solr';
+
+    public static function toHtml5DateFormat(DateTime $date)
+    {
+        return $date->format('Y-m-d');
+    }
+
+    public static function toHtml5DateTimeFormat(DateTime $date)
+    {
+        return $date->format('Y-m-d\TH:i');
+    }
+
+    public static function toMysqlDateTimeFormat(DateTime $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public static function toSolrDateTimeFormat(DateTime $date)
+    {
+        return $date->format('Y-m-d\TH:i:s.z\Z');
+    }
+
+    public static function toTimeAgoFormat(DateTime $date)
+    {
+        $diff_seconds = time() - $date->format('U');
+        $diff_weeks = floor($diff_seconds / 604800);
+        $diff_seconds -= $diff_weeks * 604800;
+        $diff_days = floor($diff_seconds / 86400);
+        $diff_seconds -= $diff_days * 86400;
+        $diff_hours = floor($diff_seconds / 3600);
+        $diff_seconds -= $diff_hours * 3600;
+        $diff_minutes = floor($diff_seconds / 60);
+        $diff_seconds -= $diff_minutes * 60;
+
+        if ($diff_days + $diff_weeks <= 0) {
+            if ($diff_hours <= 0) {
+                $str = $diff_minutes . ' minutes ago';
+            } else {
+                $str = $diff_hours . ' hours, ' . $diff_minutes . ' minutes ago';
+            }
+        } elseif ($diff_days == 1
+            && $diff_weeks <= 0
+        ) {
+            $str = 'Yesterday, ' . $date->format('g:i A');
+        } elseif ($diff_weeks < 1) {
+            $str = $date->format('l, g:i A');
+        } elseif ($diff_weeks == 1) {
+            $str = 'Last week, ' . $date->format('l');
+        } elseif ($diff_weeks < 4) {
+            $str = $diff_weeks . ' weeks ago, ' . $date->format('l');
+        } else {
+            $str = $date->format('F jS, Y');
+        }
+
+        return $str;
+    }
 
     public function format($format)
     {
-        if (0 === strcmp($format, self::FORMAT_HTML5_DATE)) {
-            return parent::format('Y-m-d');
-        }
-
-        if (0 === strcmp($format, self::FORMAT_HTML5_DATETIME)) {
-            return parent::format('Y-m-d\TH:i');
-        }
-
-        if (0 === strcmp($format, self::FORMAT_MYSQL)) {
-            return parent::format('Y-m-d H:i:s');
-        }
-
-        if (0 === strcmp($format, self::FORMAT_RELATIVE)) {
-            $diff_seconds = time() - parent::format('U');
-            $diff_weeks = floor($diff_seconds / 604800);
-            $diff_seconds -= $diff_weeks * 604800;
-            $diff_days = floor($diff_seconds / 86400);
-            $diff_seconds -= $diff_days * 86400;
-            $diff_hours = floor($diff_seconds / 3600);
-            $diff_seconds -= $diff_hours * 3600;
-            $diff_minutes = floor($diff_seconds / 60);
-            $diff_seconds -= $diff_minutes * 60;
-
-            if ($diff_days + $diff_weeks <= 0) {
-                if ($diff_hours <= 0) {
-                    $contextualDttmString =
-                        $diff_minutes . ' minutes ago';
-                } else {
-                    $contextualDttmString =
-                        $diff_hours . ' hours, '
-                        . $diff_minutes . ' minutes ago';
-                }
-            } elseif ($diff_days == 1
-                && $diff_weeks <= 0
-            ) {
-                $contextualDttmString =
-                    'Yesterday, '
-                    . parent::format('g:i A');
-
-            } elseif ($diff_weeks < 1) {
-                $contextualDttmString =
-                    parent::format('l, g:i A');
-            } elseif ($diff_weeks == 1) {
-                $contextualDttmString =
-                    'Last week, '
-                    . parent::format('l');
-            } elseif ($diff_weeks < 4) {
-                $contextualDttmString =
-                    $diff_weeks . ' weeks ago, '
-                    . parent::format('l');
-            } else {
-                $contextualDttmString =
-                    parent::format('F jS, Y');
-            }
-
-            return $contextualDttmString;
-        }
-
-        if (0 === strcmp($format, self::FORMAT_SOLR)) {
-            return parent::format('Y-m-d\TH:i:s.z\Z');
+        switch ($format) {
+            case self::FORMAT_HTML5_DATE:
+                return self::toHtml5DateFormat($this);
+            case self::FORMAT_HTML5_DATETIME:
+                return self::toHtml5DateTimeFormat($this);
+            case self::FORMAT_MYSQL:
+                return self::toMysqlDateTimeFormat($this);
+            case self::FORMAT_RELATIVE:
+            case self::FORMAT_TIMEAGO:
+                return self::toTimeAgoFormat($this);
+            case self::FORMAT_SOLR:
+                return self::toSolrDateTimeFormat($this);
         }
 
         return parent::format($format);
